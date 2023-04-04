6eb2cc33b1b6be817b1fe94f74041e2ccbb8df5e
AjayRamakrishnan146 <129526798+AjayRamakrishnan146@users.noreply.github.com>
2023-04-04 17:56:39 +0530

    Create challenge1.js

---
+++ challenge1.js
@@ -0,0 +1,13 @@
+var i;
+var n;
+for(i=1;i<=15;i++){
+    n=i%2;
+    if(n==0){
+        console.log(i,"is an even number");
+    }
+    else {
+        console.log(i,"is an odd number");
+    }
+
+    
+}
